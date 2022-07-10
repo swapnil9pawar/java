8b09199e5acf8822c675487044492d1c0eb72eb2
swapnil9pawar <104321535+swapnil9pawar@users.noreply.github.com>
2022-07-10 11:44:07 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Jaaaavjggjkaswapnifffffkucl");  
+     System.out.println("Hello Jaaaavjggjkaswapnffffifffffkucl");  
     }  
 }  
